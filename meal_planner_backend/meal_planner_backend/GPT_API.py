8450f69f934b68f12dc776c94833908a4d30f377
AdaSawilska <01143808@pw.edu.pl>
2025-01-13 20:33:42 +0100

    API try

---
+++ meal_planner_backend/meal_planner_backend/GPT_API.py
@@ -0,0 +1,45 @@
+from openai import OpenAI
+
+def chat_with_gpt(prompt, key):
+    """
+    Funkcja do komunikacji z modelem GPT przez API.
+
+    Args:
+        prompt (str): Treść zapytania wysyłanego do modelu.
+        key (str): Klucz do API.
+
+    Returns:
+        str: Odpowiedź wygenerowana przez model.
+    """
+    client = OpenAI(api_key=key)
+
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            store=True,
+            messages=[
+                {"role": "user", "content": prompt}
+            ]
+        )
+
+        return response.choices[0].message.content
+
+    except Exception as e:
+        return f"Błąd podczas komunikacji z API: {e}"
+
+
+def read_key(file_path):
+    with open(file_path, 'r') as file:
+        key = file.read()
+    return key
+
+def main(API_KEY, prompt):
+        response = chat_with_gpt(prompt, API_KEY)
+        print(f"GPT: {response}")
+
+
+if __name__ == "__main__":
+    API_KEY = read_key("key.txt")
+    prompt = "Tell me what I can do for dinner in less than 20 minutes"
+    main(API_KEY, prompt)
+
